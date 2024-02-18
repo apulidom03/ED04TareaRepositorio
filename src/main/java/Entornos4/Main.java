c62fb3f82aec58da4fe4a17d1363c8d055736189
angel <angel@DESKTOP-URT1RBH>
2024-02-18 17:57:10 +0100

    ED04TareaCommit1

---
+++ src/main/java/Entornos4/Main.java
@@ -0,0 +1,29 @@
+package Entornos4;
+
+public class Main {
+    public static void main(String[] args) {
+        NuevoMetodo();
+    }
+
+    public static void NuevoMetodo() {
+        eCesta cesta;
+        cesta = new eCesta("Pulido Martínez", 100, "20509007E", 10);
+
+        try {
+            cesta.comprar(50);
+
+        } catch (Exception e) {
+            System.out.println("No se puede pagar  ");
+        }
+
+        try {
+            System.out.println("Solicitar actualización de bono");
+            cesta.ActualizarBono(100);
+        } catch (Exception e) {
+            System.out.println("Error al recargar");
+        }
+
+        double bonoactual = cesta.bonificacion();
+        System.out.println("Su bono actual es  " + bonoactual);
+    }
+}
